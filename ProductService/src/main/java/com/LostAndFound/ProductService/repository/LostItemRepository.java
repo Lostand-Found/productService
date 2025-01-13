1dfe93c4d6c39646bf3d25ab5bea006579b58358
IshikaJaiswal202 <jishika202@gmail.com>
2025-01-13 13:14:40 +0530

    remove unused code from product service (#4)
    Co-authored-by: sarita gourh <161437473+sarita123gourh@users.noreply.github.com>

--- ProductService/src/main/java/com/LostAndFound/ProductService/repository/LostItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.LostAndFound.ProductService.repository;
-
-import com.LostAndFound.ProductService.entity.LostItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LostItemRepository extends JpaRepository<LostItem,Integer> {
-}
